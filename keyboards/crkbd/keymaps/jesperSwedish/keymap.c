fe41364789e1badb741de896082d291fa1cd9802
Jesper Hellberg <jj.hellberg@gmail.com>
2023-07-19 20:42:20 +0200

    rebase upstream and rename file

--- keyboards/crkbd/keymaps/jesperSwedish/keymap.c
+++ keyboards/crkbd/keymaps/jesperSwedish/keymap.c
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include <stdio.h>
-#include "keymap_intl.h"
+#include "keycode_aliases.h"
 
 bool lshift = false;
 bool rshift = false;
