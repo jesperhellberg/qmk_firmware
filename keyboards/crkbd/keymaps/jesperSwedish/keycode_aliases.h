dde38f76254c970db751baee60f3a367ec005dac
Jesper Hellberg <jj.hellberg@gmail.com>
2023-07-19 20:42:20 +0200

    rebase upstream and rename file

--- keyboards/crkbd/keymaps/jesperSwedish/keycode_aliases.h
+++ keyboards/crkbd/keymaps/jesperSwedish/keycode_aliases.h
@@ -4,7 +4,6 @@
 #include "keymap_swedish.h"
 
 #define OSM_SHIFT OSM(MOD_RSFT)
-//#define OSM_CTRL OSM(MOD_LCTL)
 #define GUI_ENT GUI_T(KC_ENT)
 #define OSM_ALT OSM(MOD_LALT)
 #define GUI_SCL GUI_T(KC_SCLN)
@@ -15,17 +14,11 @@
 #define SYM_DEL LT(SYMBOL,KC_DEL)
 #define SYM_BSPC LT(SYMBOL,KC_BSPC)
 
-// ***** swedish chars
-//#define SE_A ALGR(KC_A)
-//#define SE_AE ALGR(KC_D)
-//#define SE_O ALGR(KC_S)
-
 
 // ***** IDEA
 #define IDEBUG A(S(KC_F9))
 #define IRUN A(S(KC_F10))
 #define IFORMAT A(C(S(KC_L)))
-//#define ICLASS C(S(KC_N))
 #define ICLASS C(KC_N)
 #define GOSUP C(S(A(KC_U)))
 
